bf22eaeb9d22707060ea957a43e6fba9194103e2
shubham79 <shubhamrocks0709@gmail.com>
2018-01-13 19:49:10 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+var app = angular.module( 'myApp', [] );
+
+app.directive( 'editInPlace', function() {
+  return {
+    restrict: 'E',
+    scope: { value: '=' },
+    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
+    link: function ( $scope, element, attrs ) {
+     var inputElement = angular.element( element.children()[1] );
+     element.addClass( 'edit-in-place' );
+     $scope.edit = function () {
+        $scope.editing = true;
+        element.addClass( 'active' );
+        inputElement[0].focus();
+     };
+      
+      
+     inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+     });
+    }
+  };
+});
+
+app.controller('TodoCtrl', function ( $scope ) {
+  $scope.todos = [
+    {text:'Learn AngularJS', done:false},         
+    {text: 'Build an app', done:false}
+  ];
+  
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+  
+  
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+  
+  
+    $scope.clearCompleted = function () {
+		console.log($scope.todos);
+        for(var i=0; i<$scope.todos.length ; i++)
+		{
+			if($scope.todos[i].done === true)
+			{
+				console.log("in");
+				$scope.todos.splice(i,1);
+			}
+		}
+		console.log($scope.todos);
+	};
+});
